35af9d17bc89b4cc02119128054afeaaa1741336
shawnhe <shawnhe@gmail.com>
2019-09-11 05:51:05 -0700

    Update test-aws.sh

--- bin/test-aws.sh
+++ bin/test-aws.sh
@@ -2,5 +2,5 @@
 
 frame=$(cat ../config/frame.blob)
 
-curl -s -X POST -H "Content-Type: application/json" --data-binary '{"frame":"'"${frame}"'", "threshold":"75"}' http://3.218.29.81:8080/findTheCats
+curl -s -X POST -H "Content-Type: application/json" --data-binary '{"frame":"'"${frame}"'", "threshold":"45"}' http://3.218.29.81:8080/findTheCats/kmp
 
