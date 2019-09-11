4e3f8e370b7635c60228b1bfc3c76a9223478549
shawnhe <shawnhe@gmail.com>
2019-09-10 23:34:08 -0700

    Create test-aws2.sh

---
+++ bin/test-aws2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+frame=$(cat ../config/frame.blob)
+
+curl -s -X POST -H "Content-Type: application/json" --data-binary '{"frame":"'"${frame}"'"}' http://3.218.29.81:8080/findTheCats
+
