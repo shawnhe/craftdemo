e6f2441f10033cd1d326780a0793db059b80a46a
shawnhe <shawnhe@gmail.com>
2019-09-11 20:36:20 -0700

    remove old tests

--- bin/test-aws.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-frame=$(cat ../config/frame.blob)
-
-curl -s -X POST -H "Content-Type: application/json" --data-binary '{"frame":"'"${frame}"'", "threshold":"50"}' http://3.218.29.81:8080/findTheCats/kmp
-
--- bin/test-local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-frame=$(cat ../config/frame.blob)
-
-curl -s -X POST -H "Content-Type: application/json" --data-binary '{"frame":"'"${frame}"'", "threshold":"45"}' http://localhost:8080/findTheCats/kmp
-
